f4c9bc0ac09b7c0e42d483e1075585bd87970d4a
Víctor Porcar <victormpcmun@gmail.com>
2022-11-09 07:41:08 +0100

    improvement

--- bookmarks/generate.cmd
+++ bookmarks/generate.cmd
@@ -1,21 +1,24 @@
 @echo off
 
-:: please set this variable properly for your environment
+set "THIS_FOLDER=%~dp0"
+
+:: ======================================================
 
 set "BOOKMARK2MD_HOME=D:\VICTOR-MASTER\FSD\MY_PROJECTS\GitHub\Bookmark2md"
-set "BOOKMARK_EXPORTED_FROM_BROWSER_DIRECTORY=D:\VICTOR-MASTER\Bookmarks"
-
-:: ======================================================
-
-
-set "PARENT_PATH=%~dp0"
+set "BOOKMARK_EXPORTED_FROM_BROWSER_DIRECTORY=D:\VICTOR-MASTER\Bookmarks\RawBookmarks"
 set "BOOKMARK_FOLDER_NAME=IT"
-set "GENERATED_FOLDER=%PARENT_PATH%generated"
-set "MARKDOWN_FILE_NAME=generated_MD_FSD.md"
-set "HTML_PRETTY_FILE_NAME=generated_HTML_FSD.html"
-set "RAW_IMPORTABLE_HTML_FILE=bookmarksFSD.html"
 
 :: ======================================================
+
+:: CALCULATE PARAMETERS FOR bookmark2md
+
+
+set "GENERATED_FOLDER=%THIS_FOLDER%generated"
+set "MARKDOWN_FILE_NAME=generated_MD_%BOOKMARK_FOLDER_NAME%.md"
+set "HTML_PRETTY_FILE_NAME=generated_PRETTY_HTML_%BOOKMARK_FOLDER_NAME%.html"
+set "RAW_IMPORTABLE_HTML_FILE=bookmarks%BOOKMARK_FOLDER_NAME%.html"
+
+:: =========================================================================================
 
 
 for /f %%i in ('dir %BOOKMARK_EXPORTED_FROM_BROWSER_DIRECTORY%  /b/a-d/od/t:c') do set LATEST_BOOKMARK_EXPORTED_FILE=%%i
