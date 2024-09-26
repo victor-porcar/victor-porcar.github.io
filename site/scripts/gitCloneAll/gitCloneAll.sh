e1cade620d558794698e72f323beaa0136350ca3
Víctor Porcar <56938399+victor-porcar@users.noreply.github.com>
2024-09-26 12:56:01 +0200

    Update gitCloneAll.sh

--- site/scripts/gitCloneAll/gitCloneAll.sh
+++ site/scripts/gitCloneAll/gitCloneAll.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo "Usage gitCloneAll.sh <REPO_NAME> <LOCAL_DIRECTORY> <AUTHENTICATION_GITHUB_TOKEN>"
+    echo "Usage gitCloneAll.sh <USER_NAME> <LOCAL_DIRECTORY> <AUTHENTICATION_GITHUB_TOKEN>"
     echo "Example: gitCloneAll.sh victor-porcar \"/home/victor.porcar/my_repository\" ghp_MpcYKKKdlqFQirdH15JcH3hwia45B4265DzQ"
     exit 1
 else
@@ -22,8 +21,6 @@ fi
 
 cd $dir
 
-curl "https://api.github.com/users/$name/repos?page=1&per_page=1000" --header "Authorization: Bearer $GITHUB_TOKEN" | grep -e 'clone_url*' | cut -d \" -f 4 | xargs -L1 git clone
+curl "https://api.github.com/user/repos" --header "Authorization: Bearer $GITHUB_TOKEN" | grep -e 'clone_url*' | cut -d \" -f 4 | sed  "s/https:\/\//https:\/\/$GITHUB_TOKEN@/g" |xargs -L1 git clone
  
- 
-
 exit 0
