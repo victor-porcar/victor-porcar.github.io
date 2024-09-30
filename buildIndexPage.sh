0c10d69ea61518c7c62cfc5530068980621257e1
victor.porcar <victor.porcar@mirada.tv>
2024-09-30 15:45:47 +0200

    goo

--- buildIndexPage.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-./site/scripts/bookmarks/updateBookmarks.sh
-
-cp ./header.md index.md
-cat ./site/bookmarks/generated_MD_IT.md >> index.md
--- site/bookmarks/updateBookmarks.sh
+++ site/bookmarks/updateBookmarks.sh
@@ -5,7 +5,7 @@ THIS_FOLDER=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 # please set this variable properly for your environment
 
 #BOOKMARK2MD_HOME="$HOME/victormpcmun/Bookmark2md"
-BOOKMARK2MD_HOME="$THIS_FOLDER/../../../../Bookmark2md"
+BOOKMARK2MD_HOME="$THIS_FOLDER/../../../Bookmark2md"
 
 BOOKMARK_EXPORTED_FROM_BROWSER_DIRECTORY="$HOME/Bookmarks"
 BOOKMARK_FOLDER_NAME="IT"
@@ -16,7 +16,7 @@ BOOKMARK_FOLDER_NAME="IT"
 
 
 
-GENERATED_FOLDER="$THIS_FOLDER/../../bookmarks"
+GENERATED_FOLDER="$THIS_FOLDER/generated"
 MARKDOWN_FILE_NAME="generated_MD_$BOOKMARK_FOLDER_NAME.md"
 HTML_PRETTY_FILE_NAME="generated_PRETTY_HTML_$BOOKMARK_FOLDER_NAME.html"
 RAW_IMPORTABLE_HTML_FILE="bookmarks$BOOKMARK_FOLDER_NAME.html"
