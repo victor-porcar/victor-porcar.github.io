d855860d77811ff9b4eca45d7581735719025e5a
victor.porcar <victor.porcar@mirada.tv>
2024-10-29 10:01:27 +0100

    Automated commit: Updates from script

--- bookmarkUpdatePullCommitPush.sh
+++ bookmarkUpdatePullCommitPush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -n 1 -s -r -p "Export Bookmarks from browser into $HOME/Bookmarks"
+read -n 1 -s -r -p "Export Bookmarks from browser into $HOME/Bookmarks and PUSH ANY KEY"
 
 ./site/my-bookmarks/updateBookmarks.sh
 
