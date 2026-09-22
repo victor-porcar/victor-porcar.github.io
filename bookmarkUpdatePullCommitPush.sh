f1ead187b44efd1dae6e1c0105a79fbfdedb0e8b
victor-porcar <victormpcmun@gmail.com>
2026-09-22 08:24:53 +0200

    Remove bookmarkUpdatePullCommitPush.sh, replaced by generate-it-bookmarks

--- bookmarkUpdatePullCommitPush.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -n 1 -s -r -p ">>> Export Bookmarks from browser into $HOME/Bookmarks and PUSH ANY KEY <<<"
-
-./site/my-it-bookmarks/updateBookmarks.sh
-
-./site/my-scripts/gitPullCommitPush/gitPullCommitPush.sh "./"
